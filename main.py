0ebdb40044a885a96f9a02d10f9436a174ad4210
john <jfitzgerald1126@gmail.com>
2023-07-28 13:29:42 -0400

    Add wait to button

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from login_utils import login
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 if __name__ == '__main__':
     driver = login()
 
-    button_to_click = driver.find_element_by_xpath('/html/body/div/div/div[2]/div/div[2]/div[1]/div/div[2]/div[1]/div[2]/div[1]/div/div[1]/div[2]/div[1]/div/div[1]/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[1]/div/div[2]/div/div/div[1]/div/div')
-
-    # /html/body/div/div/div[2]/div/div[2]/div[1]/div/div[2]/div[1]/div[2]/div[1]/div/div[1]/div[2]/div[1]/div/div[1]/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[1]/div/div[2]/div/div/div[1]/div/div
+    button_to_click = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, ''))
+    )
 
     # schedule click ...
     # or click once and schedule script
+
+    button_to_click.click()
 
     driver.quit()
