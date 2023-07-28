d8290219ee8cce57ede5d03f30a5085cf322adc6
john <jfitzgerald1126@gmail.com>
2023-07-28 13:29:23 -0400

    Add wait for webpage to load to login and remove hard code path

--- login_utils.py
+++ login_utils.py
@@ -1,5 +1,9 @@
 import os
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def login():
     
@@ -8,16 +12,21 @@ def login():
     password = os.environ.get('APP_PASSWORD')
     login_url = os.environ.get('LOGIN_URL')
 
-    driver = webdriver.Chrome('chromedriver.exe')
+    service = Service(executable_path='C:\chromedriver\chromedriver.exe')
+    driver = webdriver.Chrome(service=service)
     driver.get(login_url)
 
-    username_input = driver.find_element_by_xpath('//*[@id="x-auto-1-input"]')
-    password_input = driver.find_element_by_xpath('//*[@id="x-auto-2-input"]')
+    username_input = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.XPATH, ''))
+    )
+    password_input = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.XPATH, ''))
+    )
 
     username_input.send_keys(username)
     password_input.send_keys(password)
 
-    login_button = driver.find_element_by_xpath('/html/body/div/div/div[2]/div/div[3]/div/div')
+    login_button = driver.find_element('xpath', '')
     login_button.click()
 
     return driver
