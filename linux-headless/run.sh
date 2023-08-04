e55b122e749c5ff56ee245a456364a91fe93a8b4
john <jfitzgerald1126@gmail.com>
2023-08-04 10:14:56 -0400

    Extend linux wrapper script to include cron usage

--- linux-headless/run.sh
+++ linux-headless/run.sh
@@ -4,3 +4,7 @@ export APP_PASSWORD=[your_password]
 export LOGIN_URL=[your_login_url]
 
 # your script here
+
+# e.g. for cron job
+cd /[your_path_to_app_directory]/linux-headless 
+python3 main.py >> [your_logging_file]
